c51c59895025c43d3894814740c9ef7956ec369a
Mattia Sotgia <mattiasotgia01@gmail.com>
2022-11-17 15:18:56 +0100

    FP stage

---
+++ PrismScheme.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TCanvas.h>
+#include <TRandom3.h>
+#include <TApplication.h>
+
+using namespace std;
+
+namespace ex{
+  double alpha   = M_PI/3;
+  double lambda1 = 579.1;
+  double lambda2 = 404.7;
+  double th0   = 1.5701;
+  double th1   = 2.6958;
+  double th2   = 2.7904;
+  double eth   = 0.0005;
+}
+
+void GetAB(double th0, double th1, double th2, double &A, double &B){
+  //  A  = ..
+  //  B  = ..
+}
+
+int main(){
+
+  return 0;
+}
---
+++ PrismScheme.py
@@ -0,0 +1,19 @@
+from   ROOT import *
+from   math import *
+import time 
+
+#Parametri e misure
+lambda1 = 579.1
+lambda2 = 404.7
+alpha   = pi/3
+th0     = 1.5701
+th1     = 2.6958
+th2     = 2.7904
+eth     = 0.0005
+
+#Funzione 
+def GetAB(th0,th1,th2):
+#   ...
+    return A,B
+
+#Main
